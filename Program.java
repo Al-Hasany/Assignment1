5eaa2c2d752a98c502b543356f91a276717f86b3
Al-Hasany <153916871+Al-Hasany@users.noreply.github.com>
2023-12-15 15:47:08 +0300

    Create Program.java

---
+++ Program.java
@@ -0,0 +1,25 @@
+package com.arzero12.program;
+
+public class Assignment1 {
+
+    public static void main(String[] args) {
+        // Question1
+        int x = 4;
+        int y = 8;
+
+        System.out.println("The result of adding is: " + (x + y));
+        System.out.println("The result of subtraction is: " + (x - y));
+        System.out.println("The result of multiplication is: " + (x * y));
+        System.out.println("The result of division is: " + (x / y));
+        // Question2
+        String firstName = "Abdullah";
+        int age = 18;
+        double weight = 46.5;
+        char grade = 'A';
+        
+        System.out.println("My name is: " + firstName);
+        System.out.println("My age is: " + age);
+        System.out.println("My weight is: " + weight + "kg");
+        System.out.println("The grade that I will get in this class is: " + grade);
+    }
+}
